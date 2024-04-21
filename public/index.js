d5171660e1aae8430f292643c4d9a23c0862fd04
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-21 12:49:53 +0300

    Add images

---
+++ public/index.js
@@ -0,0 +1,6 @@
+import bigImg from "./big.png";
+import dangerImg from "./danger.png";
+import frigoImg from "./frigo.png";
+import truckImg from "./truck.png";
+
+export { bigImg, dangerImg, frigoImg, truckImg };
