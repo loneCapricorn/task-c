1bf0ec9135b4c2d3dd2daeafcc46a0fae19f23f1
loneCapricorn <mehmed.yunuz.a55@learn.telerikacademy.com>
2024-04-21 16:52:24 +0300

    Add Pros section

--- src/app/sections/Pros.tsx
+++ src/app/sections/Pros.tsx
@@ -1,7 +1,14 @@
+import Agenda from "../components/Agenda";
+
 const Pros = () => {
   return (
-    <div>Pros</div>
-  )
-}
+    <section className="max-sm:text-sm">
+      <h2 className="font-grotesk text-2xl sm:text-4xl lg:text-5xl xl:text-6xl font-semibold ">
+        Предимства
+      </h2>
+      <Agenda />
+    </section>
+  );
+};
 
-export default Pros
+export default Pros;
